6189005f6722365081dece89150ad23c9657a93c
fulhaiyu-iso51-art <fulha.i.yu.-iso51@edu.kpi.ua>
2026-04-21 08:36:49 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // Асоціація
+        var course = new Course("C#");
+        course.AddStudent(new Student("Іван"));
+        course.AddStudent(new Student("Оля"));
+        course.ShowStudents();
+
+        Console.WriteLine();
+
+        // Композиція
+        var car = new Car("BMW", "V8");
+        car.Drive();
+
+        Console.WriteLine();
+
+        // Агрегація
+        var library = new Library(new List<Book>
+        {
+            new Book("C# Basics"),
+            new Book("OOP Guide")
+        });
+
+        var book = library.Find("C# Basics");
+        Console.WriteLine(book != null ? "Знайдено: " + book.Title : "Не знайдено");
+
+        Console.WriteLine();
+
+        // Абстракція
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+
+        dog.MakeSound();
+        cat.MakeSound();
+    }
+}
